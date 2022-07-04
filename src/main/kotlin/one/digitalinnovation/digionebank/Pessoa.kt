c57e142fd29df5fd256fad899ee875a4334bdfde
math.chiarello <chiarellomatheus@gmail.com>
2022-07-04 19:38:21 -0300

    atualizado class Pessoa.kt

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,11 +2,10 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Jether"
-    var cpf: String = "123.123.132-12"
 
-    inner class Endereco {
-        var rua:String = "Rua Teste"
-    }
+    var cpf: String = "123.123.132-12"
+        private set
+
 }
 
 fun main() {
@@ -14,7 +13,6 @@ fun main() {
 
     println(jether.nome)
     println(jether.cpf)
-    println(jether.Endereco().rua)
 
 
 }
