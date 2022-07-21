a25ae9cbcc94b25790695b8055221508067b4a44
math.chiarello <chiarellomatheus@gmail.com>
2022-07-20 21:59:49 -0300

    creates enumTipoCliente

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,22 +1,8 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Jether"
-    var cpf: String = "123.123.132-12"
-        private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome = $cpf"
-
-}
-
-fun main() {
-    val jether = Pessoa()
-
-    println(jether.nome)
-    println(jether.cpf)
-    println(jether.pessoaInfo())
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+    )
 
 
-}
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Pessoa
+
+fun main() {
+    val jether = Pessoa("Jether","12123123")
+
+    println(jether.nome)
+    println(jether.cpf)
+
+
+}
